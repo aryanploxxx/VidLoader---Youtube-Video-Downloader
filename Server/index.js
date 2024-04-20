486b456d74aba885683a68a429fbcc4960c83e8a
aryanploxxx <aryangupta8291@gmail.com>
2024-04-20 17:38:15 +0530

    Server Setup Complete

---
+++ Server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const ytdl = require('ytdl-core')
+
+const app = express()
+var cors = require("cors");
+app.use(cors());
+require("dotenv").config()
+
+const PORT = process.env.PORT
+
+app.get('/download', async (req, res) => {
+    try {
+        const url = req.query.url
+        const videoId = await ytdl.getURLVideoID(url)
+        const metaInfo = await ytdl.getInfo(url)
+        let data = {
+            url: 'https://www.youtube.com/embed/'+videoId,
+            info: metaInfo.formats
+        }
+        return res.send(data)
+    } catch(error) {
+        return res.status(500)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`)
+})
